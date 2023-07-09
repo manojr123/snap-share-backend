53b4be79f93dcc06fbc91c25df78f9a081e8c362
manojr123 <manojraghavan2008@yahoo.co.in>
2023-07-10 00:05:08 +0530

    Updated comments

---
+++ README.md
@@ -0,0 +1,21 @@
+# SnapShare Backend
+
+## Requirement
+   - Create a simple React application that displays a random image in the center of the page each time the page is refreshed. 
+   - Include a share button under the image that allows users to share the current image on Facebook, Twitter, and WhatsApp. 
+   - The image displayed on the page should be visible as a preview when the URL is shared on these platforms.
+
+
+##
+
+**Getting started :**
+  - Fork the project
+  - Clone the forked repository in your local system
+  - Update the .environment file in the /config directory and update the variables 
+    as required.
+  - Install all required packages
+      - npm install
+  - Run project
+      - npm start
+
+  The project will be running on port 8020 (can be customized in the .environment file).
--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+/**
+ * @author Manoj Raghavan
+ * @version 0.0.1
+ * 09-07-2023 - Creation
+ * 
+ * Snap Share App  :
+ *   A React application that displays a random image in the center of the page 
+ *   each time the page is refreshed. a share button under the image that allows 
+ *   users to share the current image on Facebook, Twitter, and WhatsApp. The 
+ *   image displayed on the page should be visible as a preview when the URL is 
+ *   shared on these platforms.
+ * 
+ *   This node server serves as the Snap Share Backend for rendering the
+ *   html with meta data and the random snap URL 
+ */
+
+
+
 const express = require('express');
 const cors = require("cors")
 const port = 8080;
@@ -25,15 +43,20 @@ console.log('__dirname ', __dirname);
 const indexPath  = path.resolve(__dirname, 'index.html');
 console.log('indexPath ', indexPath);
 
+
+/* Controller for "/" end point
+   - The url for the random snap is sent in the query param
+
+*/
 app.get('/', function(req,res) {
-    console.log("res.body.url", req.body.url);
+   
     console.log("res.query.url", req.query);
     fs.readFile(indexPath, 'utf8', (err, htmlData) => {
         if (err) {
             console.error('Error during file reading', err);
             return res.status(404).end()
         }
-        // get post info
+        // get url info
         const url = req.query.url;
         
         htmlData = htmlData.replace(
@@ -51,16 +74,8 @@ app.get('/', function(req,res) {
 
     });
 
-    //res.send('<h1>Server is running<h1>');
 })
 
-// app.get('/', function(req,res) {
-//     console.log("res.body.url", req.body.url);
-//     console.log("res.query.url", req.query);
-
-//     res.send('<h1>Server is running<h1>');
-//     //res.send(htmlData);
-// })
 
 app.listen(port, function(err) {
     if ( err) {
