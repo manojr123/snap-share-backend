62ee9d95956b03fca7e45b090d0b306df4ac996a
manojr123 <manojraghavan2008@yahoo.co.in>
2023-07-09 21:09:43 +0530

    Updated code to read from res.query params

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ console.log('__dirname ', __dirname);
 const indexPath  = path.resolve(__dirname, 'index.html');
 console.log('indexPath ', indexPath);
 
-app.post('/', function(req,res) {
+app.get('/', function(req,res) {
     console.log("res.body.url", req.body.url);
     console.log("res.query.url", req.query);
     fs.readFile(indexPath, 'utf8', (err, htmlData) => {
@@ -34,7 +34,7 @@ app.post('/', function(req,res) {
             return res.status(404).end()
         }
         // get post info
-        const url = req.body.url;
+        const url = req.query.url;
         
         htmlData = htmlData.replace(
             `<meta property="og:image" content="https://picsum.photos/250/250">`,
@@ -54,13 +54,13 @@ app.post('/', function(req,res) {
     //res.send('<h1>Server is running<h1>');
 })
 
-app.get('/', function(req,res) {
-    console.log("res.body.url", req.body.url);
-    console.log("res.query.url", req.query);
+// app.get('/', function(req,res) {
+//     console.log("res.body.url", req.body.url);
+//     console.log("res.query.url", req.query);
 
-    res.send('<h1>Server is running<h1>');
-    //res.send(htmlData);
-})
+//     res.send('<h1>Server is running<h1>');
+//     //res.send(htmlData);
+// })
 
 app.listen(port, function(err) {
     if ( err) {
